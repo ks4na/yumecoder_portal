e00496a67909852f29eacf371710dd6478e1b979
ks4na <1017086648@qq.com>
2019-12-10 00:49:23 +0800

    添加Intl的polyfill兼容IE9,10,safari;优化多语言获取方法的逻辑

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,27 +2,28 @@ import enUS from './en-US.js'
 import jaJP from './ja-JP.js'
 import zhCN from './zh-CN.js'
 
-const locales = {
-  'zh-cn': zhCN,
-  'en-us': enUS,
-  'ja-jp': jaJP,
+const localeInfos = {
+  'zh-cn': { locale: 'zh-cn', msgs: zhCN },
+  'en-us': { locale: 'en', msgs: enUS },
+  'ja-jp': { locale: 'ja', msgs: jaJP },
 }
 
 const defaultLocale = 'zh-cn'
 
-export function getLocale() {
+export function getLocale(str) {
   const localeFromStorage =
     window.localStorage && window.localStorage.getItem('locale')
   const navigatorLang =
     window.navigator.language || window.navigator.browserLanguage
-  const locale = localeFromStorage || navigatorLang || defaultLocale
+  const locale = str || localeFromStorage || navigatorLang || defaultLocale
+
   const lowerCaseLocale = locale.toLowerCase()
-  if (locales[lowerCaseLocale]) {
-    return lowerCaseLocale
-  } else if (
-    Object.keys(locales).find(item => item.indexOf(lowerCaseLocale) !== -1)
-  ) {
-    return lowerCaseLocale
+
+  const item = Object.keys(localeInfos).find(
+    item => item.indexOf(lowerCaseLocale) !== -1
+  )
+  if (item) {
+    return localeInfos[item].locale
   }
   return defaultLocale
 }
@@ -30,17 +31,13 @@ export function getLocale() {
 export function getLocaleMessages(locale = defaultLocale) {
   let localeMessages
   const lowerCaseLocale = locale.toLowerCase()
-  if (locales[lowerCaseLocale]) {
-    localeMessages = locales[lowerCaseLocale]
+  const key = Object.keys(localeInfos).find(
+    item => item.indexOf(lowerCaseLocale) !== -1
+  )
+  if (key) {
+    localeMessages = localeInfos[key].msgs
   } else {
-    const key = Object.keys(locales).find(
-      item => item.indexOf(lowerCaseLocale) !== -1
-    )
-    if (key) {
-      localeMessages = locales[key]
-    } else {
-      localeMessages = locales[defaultLocale]
-    }
+    localeMessages = localeInfos[defaultLocale].msgs
   }
   return localeMessages
 }
--- src/models/sagas/locale.js
+++ src/models/sagas/locale.js
@@ -1,13 +1,13 @@
 import { takeEvery, all, put } from 'redux-saga/effects'
 import { CHANGE_LOCALE, setLocale } from '../actions/locale.js'
-import { saveLocale } from '../../locales/index.js'
+import { saveLocale, getLocale } from '../../locales/index.js'
 
 export function* watchChangeLocale() {
   yield takeEvery(CHANGE_LOCALE, changeLocale)
 }
 
 export function* changeLocale(action) {
-  const locale = action.payload
+  const locale = getLocale(action.payload)
   // 更新网页的<html>标签的lang属性
   document.querySelector('html').lang = locale
   // 保存到storage
