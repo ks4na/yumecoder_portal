fec42a5b1d861e0ae4257a116fc2cbb4408990d6
ks4na <1017086648@qq.com>
2019-12-11 16:23:11 +0800

    添加eslint-prettier的两个依赖包，仍然无法解决拖尾逗号规则冲突, 所以注释掉eslint对拖尾逗号的检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -19,10 +19,11 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'prettier/prettier': 'error',
     quotes: [2, 'single'],
     semi: [2, 'never'],
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
-    'comma-dangle': ['error', 'always-multiline'],
+    // 'comma-dangle': ['error', 'always-multiline'], // eslint 和 prettier 冲突，安装了两个eslint-preitter依赖包之后仍然存在问题
   },
 }
