0815480c7b4efa142f7d4bd65f0decb516ee9382
ks4na <1017086648@qq.com>
2020-04-08 10:53:27 +0800

    refactor(components): 更新 RootLayout 组件

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
-import styles from './RootLayout.scss'
+import { Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 320,
+  },
+  clearfix: {
+    '&::before': {
+      content: '""',
+      display: 'table',
+    },
+    '&::after': {
+      content: '""',
+      display: 'table',
+      clear: 'both',
+    },
+  },
+})
 
 interface PropTypes {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
 export default function RootLayout({ children }: PropTypes): JSX.Element {
+  const classes = useStyles()
   return (
-    <Container maxWidth="sm" className={styles.rootLayout}>
+    <Container disableGutters className={`${classes.root} ${classes.clearfix}`}>
       {children}
     </Container>
   )
