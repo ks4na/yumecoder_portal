5e5fdd50b44e17e15e0d0dee91ad172241360377
ks4na <1017086648@qq.com>
2020-04-20 17:49:26 +0800

    chore(axios.config.js): 添加locale信息，让后端API根据locale返回正确的言语

--- axios.config.js
+++ axios.config.js
@@ -10,3 +10,22 @@ const baseUrls = {
 // 配置 axios 的 baseURL
 axios.defaults.baseURL =
   baseUrls[process.env.NODE_ENV || 'development'] || baseUrls.development
+
+// 添加请求拦截器
+axios.interceptors.request.use(
+  function(config) {
+    // 发送请求时，携带 locale 信息
+    const locale = localStorage.getItem('locale')
+    if (locale) {
+      if (config.method === 'get') {
+        config.params.locale = locale
+      } else {
+        config.url += `?locale=${locale}`
+      }
+    }
+    return config
+  },
+  function(error) {
+    throw error
+  }
+)
